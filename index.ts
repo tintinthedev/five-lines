111a73730a8a06b488fad08958f34e7a06dc07f5
Tim <randomthecoder1557@gmail.com>
2025-10-18 16:45:34 -0300

    refactor: split `update` into smaller functions

--- index.ts
+++ index.ts
@@ -92,7 +92,7 @@ function moveVertical(dy: number) {
   }
 }
 
-function update() {
+function processInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
     if (current === Input.LEFT) moveHorizontal(-1);
@@ -100,7 +100,9 @@ function update() {
     else if (current === Input.UP) moveVertical(-1);
     else if (current === Input.DOWN) moveVertical(1);
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       if (
@@ -122,6 +124,11 @@ function update() {
       }
     }
   }
+}
+
+function update() {
+  processInputs();
+  updateMap();
 }
 
 function drawMap(g: CanvasRenderingContext2D) {
